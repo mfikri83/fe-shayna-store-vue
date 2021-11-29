7727264678176c21e5283136a6c12a0095d95b70
mfikri17 <fikri@ddtc.co.id>
2021-11-29 20:46:43 +0700

    add_page home

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HomeShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeShayna from '@/components/HomeShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenShayna from '@/components/WomenShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/PartnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: "Home",
+  components: {
+    HomeShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  },
+};
+</script>
